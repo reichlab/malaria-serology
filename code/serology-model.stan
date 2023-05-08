ebe684d80cba1c9ec10257b84ad11b3313278c72
Nicholas Reich <nick@umass.edu>
2023-05-08 14:35:27 -0400

    working model based on splines, with vignette.

--- code/serology-model.stan
+++ code/serology-model.stan
@@ -12,57 +12,53 @@ data {
   int<lower=0> N;    // number of observations
   vector[N] y;       // observed levels of a particular marker
   array[N] int age;  // observed ages
-  int<lower=0> T;    // the year in which data was collected
-  int year0;         // the year which should be treated as "timezero" in the logistic function 
-
+  int<lower=0> maxage; // something that should be larger than the max observed age 
+  matrix[maxage, 3] B; // design matrix with basis functions
 }
 
 transformed data{
-  int<lower=0> maxage = max(age);
-  array[maxage] int tvec = linspaced_int_array(maxage, T-year0-(maxage-1), T-year0);
+  // int<lower=1> n_basis = 3; // number of basis functions, hard-coded
 }
 
 // The parameters accepted by the model.
 parameters {
   // parameters for never infected lognormal antibody distribution
   real mu0;
-  real<lower=0> sigma0;
+  real<lower=0,upper=5> sigma0;
   // parameters for never infected lognormal antibody distribution
   real mu1;
-  real<lower=0> sigma1;
-  // parameters for the logistic curve for force of infection
-  real logit_alpha_l;
-  real logit_alpha_u;
-  real<lower=0> beta;
-  real<lower=0> neg_gamma;
+  real<lower=0,upper=5> sigma1;
+  
+  // parameters for the smooth spline
+  vector[3] beta; 
 }
 
 transformed parameters{
   // precompute all needed values of the S function
-  vector<lower=0,upper=1>[maxage] Svals = inv_logit(logit_alpha_l) + (inv_logit(logit_alpha_u)-inv_logit(logit_alpha_l))/(1 + beta*exp(neg_gamma*to_vector(tvec)));
+  vector<lower=0,upper=1>[maxage] Svals = inv_logit(B * beta);
   vector<lower=0,upper=1>[N] pi;
   for(n in 1:N) {
     // product involves computing the annual probabilities for the last age[n] values of the Svals vector
     pi[n] = 1 - prod(1-Svals[(maxage - age[n]+1):maxage]);
+    
   }
 }
 
 // The model 
 model {
   // priors
-  mu0 ~ normal(log(3), 0.5);
-  mu1 ~ normal(log(7), 0.5);
+  mu0 ~ normal(log(3), 0.4); // log-scale mean of never infecteds
+  mu1 ~ normal(log(7), 0.4); // log-scale mean of ever infecteds
   sigma0 ~ lognormal(0, .1);
   sigma1 ~ lognormal(0, .1);
-  logit_alpha_l ~ normal(-5, 2); // lower logistic asymptote on logit scale
-  logit_alpha_u ~ normal(0, 2); // upper logistic asymptote on logit scale
-  beta ~ lognormal(0, 2);
-  neg_gamma ~ lognormal(0,1);
+  beta[1] ~ normal(1, 0.5);  // inv_logit(1) = 0.73
+  beta[2] ~ normal(0, 0.5);  // inv_logit(0) = 0.5
+  beta[3] ~ normal(-3, 0.5);  // inv_logit(-3) = 0.05
 
   for(n in 1:N) {
     target += log_mix(pi[n],
-                      lognormal_lpdf(y[n] | mu0, sigma0),
-                      lognormal_lpdf(y[n] | mu1, sigma1));
+                      lognormal_lpdf(y[n] | mu1, sigma1),  // ever infected
+                      lognormal_lpdf(y[n] | mu0, sigma0)); // never infected
   };
 }
 
--- code/serology-sim.R
+++ code/serology-sim.R
@@ -1,61 +1,84 @@
 library(cmdstanr)
 library(posterior)
+library(splines)
+library(tidyverse)
 
 mdl <- cmdstan_model(stan_file = "code/serology-model.stan")
 
-nsim <- 10
+N <- 500       ## num of observations
+age <- rnbinom(N, mu=30, size=5)
+maxage <- max(age)+1
+n_basis <- 3
+time_vec <- seq.int(maxage)
+B <- bs(time_vec, df = n_basis)
 
-N <- 1000       ## num of observations
-age <- rnbinom(N, mu=35, size=5)
-maxage <- max(age)
-T <- 2016
-year0 <- 1990
-tvec <- (T-year0-(maxage-1)):(T-year0)
-yearvec <- year0+tvec
+message(paste('iteration', i, Sys.time()))
 
-for(i in 1:nsim){
-  message(paste('iteration', i, Sys.time()))
-  
-  ## simulate data directly in R, couldn't get prior simulation to work natively in stan
-  
-  ## priors
-  mu0 <- rnorm(1, log(3), .5)
-  mu1 <- rnorm(1, log(7), .5)
-  sigma0 <- rlnorm(1, 0, .1)
-  sigma1 <- rlnorm(1, 0, .1)
-  logit_alpha_l <- rnorm(1, -5, 3)
-  logit_alpha_u<- rnorm(1, 0, 3)
-  beta <- rlnorm(1, 0, 2)
-  neg_gamma <- rlnorm(1, 0,1)
-  
-  Svals <- boot::inv.logit(logit_alpha_l) + 
-    (boot::inv.logit(logit_alpha_u)-boot::inv.logit(logit_alpha_l))/(1 + beta*exp(neg_gamma*tvec))
-  oneminusSvals = 1-Svals
-  curve(logisticfunc(x, a_l = boot::inv.logit(logit_alpha_l), a_u = boot::inv.logit(logit_alpha_u), gamma = -neg_gamma, v=1, beta = beta), from=min(tvec), to=max(tvec))
-  
-  for(j in 1:N) {
-    pi[j] = 1 - prod(oneminusSvals[(maxage - age[j]+1):maxage])
-  }
-  z <- rbinom(N, 1, pi)
-  y <- (1-z)*rlnorm(N, mu0, sigma0) + z*rlnorm(N, mu1, sigma1)
-  
-  fit <- mdl$sample(data = list(N = N, ## num observations
-                                y = y,   
-                                age = age,  
-                                T = T,
-                                year0 = year0),
-                    chains=4,
-                    parallel_chains=4)
-  
-  pi_samples <- as_draws_df(fit$draws("pi")) |> 
-    apply(MARGIN = 2, FUN=function(x) quantile(x, probs=c(0.1, 0.9)))
-  for(k in 1:K){
-    coverage_pi[i,k] <- probs[k]>pi_samples[1,k] & probs[k]<pi_samples[2,k]
-  }
-  
-  beta_samples <- as_draws_df(fit$draws("beta")) |> 
-    apply(MARGIN = 2, FUN=function(x) quantile(x, probs=c(0.1, 0.9)))
-  for(k in 1:K){
-    coverage_beta[i,k] <- beta[k]>beta_samples[1,k] & beta[k]<beta_samples[2,k]
-  }
+
+## simulate data directly in R, couldn't get prior simulation to work natively in stan
+
+## priors
+mu0 <- rnorm(1, log(3), 0.2)
+mu1 <- rnorm(1, log(7), 0.2)
+# y1 <- exp(rnorm(1000, log(3),.2))
+# y2 <- exp(rnorm(1000, log(7),.2))
+# boxplot(y1, y2)
+sigma0 <- rlnorm(1, 0, .1)
+sigma1 <- rlnorm(1, 0, .1)
+beta <- rnorm(3, c(1, 0, -3), 0.5)
+
+Svals <- boot::inv.logit(B %*% beta)
+## for checking structure of pis
+# for(j in 1:N) {
+#   ## product involves computing the annual probabilities for the last age[j] values of the Svals vector
+#   pi[j] = 1 - prod(1-Svals[(maxage - age[j]+1):maxage])
+# }
+
+pi <- rep(NA, N)
+for(j in 1:N) {
+  pi[j] = 1 - prod(1-Svals[(maxage - age[j]+1):maxage])
 }
+z <- rbinom(N, 1, pi)
+y <- (1-z)*rlnorm(N, mu0, sigma0) + z*rlnorm(N, mu1, sigma1)
+plot(age, pi)
+
+fit <- mdl$sample(data = list(N = N, ## num observations
+                              y = y,   
+                              age = age,  
+                              maxage = maxage,
+                              B = B),
+                  chains=4,
+                  parallel_chains=4)
+
+pi_samples <- as_draws_matrix(fit$draws("pi")) |> 
+  apply(MARGIN = 2, FUN=function(x) quantile(x, probs=c(0.1, 0.5, 0.9))) |> 
+  t()
+colnames(pi_samples) <- c("q10", "q50", "q90")
+dat <- tibble(y, z, age, pi) |> 
+  bind_cols(pi_samples)
+ggplot(dat, aes(x=age)) +
+  geom_point(aes(y=pi)) +
+  geom_point(aes(y=q50), col="red", alpha=.1) +
+  geom_segment(aes(y=q10, yend=q90, xend=age), alpha=.1, col="red")
+
+mu0_samples <- as_draws_matrix(fit$draws("mu0")) |> 
+  apply(MARGIN = 2, FUN=function(x) quantile(x, probs=c(0.1, 0.5, 0.9)))|> 
+  t()
+colnames(mu0_samples) <- c("q10", "q50", "q90")
+
+mu1_samples <- as_draws_matrix(fit$draws("mu1")) |> 
+  apply(MARGIN = 2, FUN=function(x) quantile(x, probs=c(0.1, 0.5, 0.9)))|> 
+  t()
+colnames(mu1_samples) <- c("q10", "q50", "q90")
+
+mu_ests <- tibble(z = 0:1, mu = c(mu0, mu1)) |> 
+  bind_cols(rbind(mu0_samples, mu1_samples))
+
+ggplot(dat, aes(x=factor(z))) + 
+  geom_boxplot(aes(y=y)) + 
+  scale_y_log10() +
+  geom_point(data=mu_ests, aes(y=exp(q50)), color="blue", shape=3, size=2) +
+  geom_segment(data=mu_ests, aes(y=exp(q10), yend=exp(q90), xend=factor(z)), col="blue", size=2) +
+  geom_point(data=mu_ests, aes(y=exp(mu)), color="red", shape=4) 
+
+
