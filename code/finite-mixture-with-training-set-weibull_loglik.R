b28a66b1fb14df3cc54ee031f56fef6042ec8037
eycramer <ecramer@umass.edu>
2023-07-31 10:14:26 -0400

    r code with latest stan model

---
+++ code/finite-mixture-with-training-set-weibull_loglik.R
@@ -0,0 +1,89 @@
+library(cmdstanr)
+library(posterior)
+library(splines)
+library(tidyverse)
+library(loo)
+
+training_data <- read.csv("datasets/training_serology_sample.csv")
+
+mdl <- cmdstan_model(stan_file = "code/finite-mixture-weibull-loglik.stan") #code to pull out log likelihood in stan
+
+N <- nrow(training_data)     ## num of observations
+
+## for extracting weibull params, K=2
+fit_k2 <- mdl$sample(data = list(N = N, ## num observations
+                                 y = log(training_data$PfMSP119),   
+                                 K = 2),
+                     chains=4,
+                     parallel_chains=4)
+
+draws_k2 <- as_draws_matrix(fit_k2$draws(c("scale", "shape", "theta"))) |> 
+  data.frame()
+mean_params_k2 <- colMeans(draws_k2)  
+
+hist(log(training_data$PfMSP119), breaks = 100, freq=FALSE)
+dist1 <- function(x) dweibull(x, shape=mean_params_k2["shape.1."], scale=mean_params_k2["scale.1."])*mean_params_k2["theta.1."]
+dist2 <- function(x) dweibull(x, shape=mean_params_k2["shape.2."], scale=mean_params_k2["scale.2."])*mean_params_k2["theta.2."]
+curve(dist1(x) + dist2(x), add=TRUE)
+curve(dist1(x), add=TRUE, col="blue")
+curve(dist2(x), add=TRUE, col="red")
+
+hist(log(training_data$PfMSP119), breaks = 100, freq=FALSE)
+for(i in 1:500){
+  dist1 <- function(x) dweibull(x, shape=draws_k2[i,"shape.1."], scale=draws_k2[i,"scale.1."])*draws_k2[i,"theta.1."]
+  dist2 <- function(x) dweibull(x, shape=draws_k2[i,"shape.2."], scale=draws_k2[i,"scale.2."])*draws_k2[i,"theta.2."]
+  curve(dist1(x) + dist2(x), add=TRUE, col=alpha("black", 0.05))
+  curve(dist1(x), add=TRUE, col=alpha("blue", 0.05))
+  curve(dist2(x), add=TRUE, col=alpha("red", 0.05))
+}
+
+
+
+## for extracting weibull params, K=3
+fit_k3 <- mdl$sample(data = list(N = N, ## num observations
+                                 y = log(training_data$PfMSP119),   
+                                 K = 3),
+                     chains=4,
+                     parallel_chains=4)
+
+#shinystan::launch_shinystan(fit)
+
+draws_k3 <- as_draws_matrix(fit_k3$draws(c("scale", "shape", "theta"))) |> 
+  data.frame()
+mean_params_k3 <- colMeans(draws_k3)  
+
+hist(log(training_data$PfMSP119), breaks = 100, freq=FALSE)
+dist1 <- function(x) dweibull(x, shape=mean_params_k3["shape.1."], scale=mean_params_k3["scale.1."])*mean_params_k3["theta.1."]
+dist2 <- function(x) dweibull(x, shape=mean_params_k3["shape.2."], scale=mean_params_k3["scale.2."])*mean_params_k3["theta.2."]
+dist3 <- function(x) dweibull(x, shape=mean_params_k3["shape.3."], scale=mean_params_k3["scale.3."])*mean_params_k3["theta.3."]
+curve(dist1(x) + dist2(x) + dist3(x), add=TRUE)
+curve(dist1(x), add=TRUE, col="blue")
+curve(dist2(x), add=TRUE, col="red")
+curve(dist3(x), add=TRUE, col="green")
+
+hist(log(training_data$PfMSP119), breaks = 100, freq=FALSE)
+for(i in 1:500){
+  dist1 <- function(x) dweibull(x, shape=draws_k3[i,"shape.1."], scale=draws_k3[i,"scale.1."])*draws_k3[i,"theta.1."]
+  dist2 <- function(x) dweibull(x, shape=draws_k3[i,"shape.2."], scale=draws_k3[i,"scale.2."])*draws_k3[i,"theta.2."]
+  dist3 <- function(x) dweibull(x, shape=draws_k3[i,"shape.3."], scale=draws_k3[i,"scale.3."])*draws_k3[i,"theta.3."]
+  curve(dist1(x) + dist2(x) + dist3(x), add=TRUE, col=alpha("black", 0.05))
+  curve(dist1(x), add=TRUE, col=alpha("blue", 0.05))
+  curve(dist2(x), add=TRUE, col=alpha("red", 0.05))
+  curve(dist3(x), add=TRUE, col=alpha("green", 0.05))
+}
+
+
+# for extracting log likelihood 
+draws_k2_loglik <- as_draws_matrix(fit_k2$draws(c("log_lik")))
+
+loo_k2 <- loo(draws_k2_loglik, save_psis = TRUE)
+print(loo_k2)
+
+# for extracting log likelihood 
+draws_k3_loglik <- as_draws_matrix(fit_k3$draws(c("log_lik")))
+
+loo_k3 <- loo(draws_k3_loglik, save_psis = TRUE)
+print(loo_k3)
+
+loo_compare(loo_k2, loo_k3)
+
